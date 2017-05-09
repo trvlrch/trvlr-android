b88ee86e039959f88f54306f0b4bdcd5eb0b4965
chrisluc <chrisluc@students.zhaw.ch>
2017-05-09 14:59:38 +0200

    Clean up

--- app/src/main/java/ch/trvlr/trvlr/PublicChatActivity.java
+++ app/src/main/java/ch/trvlr/trvlr/PublicChatActivity.java
@@ -2,9 +2,6 @@ package ch.trvlr.trvlr;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
